b006686c3217ca9ce1cc10906d3960ad32e328cc
Michael Borde <michael.borde@ubimail.fr>
2017-07-29 00:20:54 +0200

    better map implementation

--- benchmarks/map.js
+++ benchmarks/map.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const {map} = require('../lib');
+const {map, mapSeries} = require('../lib');
 
 const count = 100000;
 
-benchmark(() => map(createPromiseFuncs(), f => f(), {concurrency: 3}), 'Map (concurrency: 3)')
-  .then(() => benchmark(() => map(createPromiseFuncs(), f => f(), {concurrency: 1}), 'Map (concurrency: 1)'));
+benchmark(() => map(createPromiseFuncs(), f => f()), 'Map (concurrency: 3)')
+  .then(() => benchmark(() => map(createPromiseFuncs(), f => f(), {concurrency: 1}), 'Map (concurrency: 1)'))
+  .then(() => benchmark(() => mapSeries(createPromiseFuncs(), f => f()), 'Map series'));
 
 function benchmark(func, context) {
   const start = new Date();
