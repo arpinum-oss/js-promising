da632bfa5e321262b520a796920e8bd44247b7a9
Michael Borde <michael.borde@ubimail.fr>
2017-07-30 13:39:58 +0200

    queue benchmarks

---
+++ benchmarks/queue.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const {Queue} = require('../lib');
+
+const count = 100000;
+
+const queue = new Queue();
+
+const start = new Date();
+return enqueueAll().then(printStatistics);
+
+function enqueueAll() {
+  for (let i = 0; i < count - 1; i++) {
+    enqueue();
+  }
+  return enqueue();
+
+  function enqueue() {
+    return queue.enqueue(() => new Date());
+  }
+}
+
+function printStatistics() {
+  const end = new Date();
+  const duration = end - start;
+  console.log(`${count} functions enqueued in ${duration} ms`);
+}
+
+// 100000 functions enqueued in 11621 ms
